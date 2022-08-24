f1ce41037e776b36a0148a98ba3b4c474241ad9f
Sauhard <sauhardshakya14@gmail.com>
2022-08-24 13:34:27 -0400

    Initial Commit

---
+++ src/main/java/com/example/studentCrud/controller/StudentController.java
@@ -0,0 +1,42 @@
+package com.example.studentCrud.controller;
+
+import com.example.studentCrud.model.Student;
+import com.example.studentCrud.service.StudentService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/student")
+@AllArgsConstructor
+public class StudentController {
+
+    private StudentService studentService;
+
+    @PostMapping
+    public ResponseEntity<Void> createStudent(@RequestBody Student student){
+        studentService.save(student);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+    @GetMapping("/listAll")
+    public  ResponseEntity<List<Student>> listAll(){
+        List<Student> allStudent = studentService.listAll();
+
+        return ResponseEntity.ok(allStudent);
+    }
+
+    @GetMapping("/getByStudentId")
+    public ResponseEntity<Student> getByStudentId(@RequestParam("id")Long id){
+        Student student = studentService.getByStudentId(id);
+        return ResponseEntity.ok(student);
+    }
+
+    @DeleteMapping("/delByStudentId")
+    public ResponseEntity<Void> delByStudentId(@RequestParam("id") Long id){
+        studentService.delByStudentId(id);
+        return new ResponseEntity<>(HttpStatus.GONE);
+    }
+}
---
+++ src/main/java/com/example/studentCrud/service/StudentService.java
@@ -0,0 +1,33 @@
+package com.example.studentCrud.service;
+
+import com.example.studentCrud.model.Student;
+import com.example.studentCrud.repository.StudentRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    public void save(Student student){
+        studentRepository.save(student);
+    }
+
+    public List<Student> listAll(){
+        List<Student> allStudents = studentRepository.findAll();
+        return allStudents;
+    }
+
+    public Student getByStudentId(Long StudentId){
+        Student student = studentRepository.findById(StudentId).orElse(new Student());
+        return student;
+    }
+
+    public void delByStudentId(Long studentId){
+       studentRepository.deleteById(studentId);
+    }
+ }
