bce291de9ea48e9900e47dfe0d1fe8089378a910
Sauhard <sauhardshakya14@gmail.com>
2022-08-29 11:33:40 -0400

    Sunny's Edit

--- src/main/java/com/example/studentCrud/repository/StudentRepository.java
+++ src/main/java/com/example/studentCrud/repository/StudentRepository.java
@@ -3,9 +3,16 @@ package com.example.studentCrud.repository;
 
 import com.example.studentCrud.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface StudentRepository  extends JpaRepository<Student, Long> {
+
+    @Query(value = "select * from student where name like 'J%'" , nativeQuery = true)
+    public List<Student> getStudentBySecLetter();
+
 
 }
